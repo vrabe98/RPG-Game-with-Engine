7277bf8d2f905ca14463404d996ba9e51493eeff
Vrabie Gabriel <vrabiegabriel@yahoo.com>
2023-06-24 13:21:31 +0300

    Updated libraries

--- RPG Game/Actions.cpp
+++ RPG Game/Actions.cpp
@@ -19,21 +19,19 @@ int Back_to_start() {
 }
 
 int Inc_map() {
-	int current_map = db->get_current_map_nr();
-	db->force_update_current_map(++current_map);
+	db->force_update_current_map(db->get_current_map_nr()+1);
 	return 1;
 }
 
 int Dec_map() {
-	int current_map = db->get_current_map_nr();
-	db->force_update_current_map(--current_map);
+	db->force_update_current_map(db->get_current_map_nr()-1);
 	return 1;
 }
 
 std::shared_ptr<actions_map> actions = std::make_shared<actions_map>(
 	std::initializer_list<actions_map::value_type>{
 		{"exit", std::bind(Exit)},
-		{"play", std::bind(Play) },
+		{"play", std::bind(Play)},
 		{"back_start", std::bind(Back_to_start) },
 		{"inc_map", std::bind(Inc_map)},
 		{"dec_map", std::bind(Dec_map)}
